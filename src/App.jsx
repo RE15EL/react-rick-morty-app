ae2d8c7b2f6ea6e7ce01a1c17c6db1604914a0fa
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 22:13:30 -0500

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
+import { Header } from "./components/Header";
 
 function App() {
-
+  const [darkTheme, setDarkTheme] =useState(false);
+  
   return (
-    <>
-      <h1 className="text-3xl font-bol">
-        Hello world!
-      </h1>
-    </>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-200 dark:bg-gray-800 text-gray-800
+       dark:text-gray-200 min-h-screen">
+        <Header darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+export function Header({darkTheme, setDarkTheme}) {
+  return (
+    <div className="p-5 flex justify-center items-center flex-wrap sm:justify-between 
+                    border-b border-gray-300 dark:border-gray-700">
+        <h1 className="dark:text-gray-300 text-bold font-bold" >Rick & Morty App</h1>
+        <button type="button" 
+                className="p-1 border shadow-md hover:shadow-lg border-gray-300 dark:border-gray-700 rounded-full"
+                onClick={()=>setDarkTheme(!darkTheme)}>
+            { darkTheme ? '☀️' : '🌙'}
+        </button>
+    </div>
+  )
+}
