7759f2b512e9344b36f218af064446e29d2e0e82
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 23:04:43 -0500

    update Layout style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <Outlet />
+    <div className="p-5 bg-gray-100 min-h-screen">
+      <Outlet />
+    </div>
   )
 }
