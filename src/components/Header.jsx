9c4bbd9fbb3ac1b477ffb3a1ef3fb11f5f2ca967
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 22:59:18 -0500

    added NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,13 @@
-import { Link } from "react-router-dom";
-
+import Navbar from "./Navbar";
 
 export function Header({darkTheme, setDarkTheme}) {
   return (
     <div className="p-5 flex justify-center items-center flex-wrap sm:justify-between 
                     border-b border-gray-300 dark:border-gray-700">
-        <div className="flex justify-between items-center space-x-5 w-screen">
+        <div className="flex flex-col sm:flex-row justify-between items-center space-x-5 w-screen gap-4">
             <h1 className="dark:text-gray-300 text-bold font-bold text-2xl" >Rick & Morty App</h1>
             
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/character">Character</Link>
-                </li>
-                <li>
-                  <Link to="location">Location</Link>
-                </li>
-                <li>
-                  <Link to="episode">Episode</Link>
-                </li>
-              </ul>
-            </nav>
+            <Navbar/>
 
             <button type="button" 
                     className="p-1 border shadow-md hover:shadow-lg border-gray-300 dark:border-gray-700 rounded-full"
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  
+    const applyClassName = ({isActive})=>[
+        isActive ? 'border-b dark:border-gray-300 border-gray-700' : '',
+        'mr-4', 
+    ].join(' ')
+
+    return (
+        <nav>
+            <ul className="flex flex-wrap flex-col sm:flex-row justify-center items-center gap-2 p-2
+                        dark:text-gray-300 text-bold text-sm ">
+                <li >
+                    <NavLink className={applyClassName} to="/character">Character</NavLink>
+                </li>
+                <li >
+                    <NavLink className={applyClassName} to="location">Location</NavLink>
+                </li>
+                <li >
+                    <NavLink className={applyClassName} to="episode">Episode</NavLink>
+                </li>
+            </ul>
+        </nav>
+  )
+}
