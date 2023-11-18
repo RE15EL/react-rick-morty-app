35df8e27953db73c26deaf5e24bf318a1516a5b0
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 22:17:16 -0500

    update header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,14 @@ export function Header({darkTheme, setDarkTheme}) {
   return (
     <div className="p-5 flex justify-center items-center flex-wrap sm:justify-between 
                     border-b border-gray-300 dark:border-gray-700">
-        <h1 className="dark:text-gray-300 text-bold font-bold" >Rick & Morty App</h1>
-        <button type="button" 
-                className="p-1 border shadow-md hover:shadow-lg border-gray-300 dark:border-gray-700 rounded-full"
-                onClick={()=>setDarkTheme(!darkTheme)}>
-            { darkTheme ? '☀️' : '🌙'}
-        </button>
+        <div className="flex justify-between items-center space-x-5 w-screen">
+            <h1 className="dark:text-gray-300 text-bold font-bold" >Rick & Morty App</h1>
+            <button type="button" 
+                    className="p-1 border shadow-md hover:shadow-lg border-gray-300 dark:border-gray-700 rounded-full"
+                    onClick={()=>setDarkTheme(!darkTheme)}>
+                { darkTheme ? '☀️' : '🌙'}
+            </button>
+        </div>
     </div>
   )
 }
