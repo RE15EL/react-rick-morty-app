1630438069eeb5cd6fe82db9089f881a0cfcab75
Reisel Valle <reiselvalle@gmail.com>
2023-11-20 11:42:41 -0500

    crad styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 
 export default function Card( {data} ) {
   return (
-    <div className="flex flex-col bg-gray-200 bg-gradient-to-r from-gray-300 to-gray-200 rounded-md">
+    <div className="flex flex-col bg-gray-200 bg-gradient-to-r hover:bg-gradient-to-l from-gray-300 to-gray-200 rounded-md hover:scale-105 transition-all hover:cursor-pointer">
         <img src={data?.image} alt={data?.name} />
         <div className="flex justify-between items-center border p-2 ">
             <h2 className="font-bold">{data?.name.length > 20 ? data?.name.substring(0,20) : data?.name }</h2>
