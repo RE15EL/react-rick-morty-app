8c6e070867dff5df69764f06880ec46a30f08748
Reisel Valle <reiselvalle@gmail.com>
2023-11-20 15:34:17 -0500

    episodes styles

--- src/pages/Episode.jsx
+++ src/pages/Episode.jsx
@@ -7,11 +7,11 @@ export default function Episode() {
 
   return (      
     <div className="h-[80vh]  grid place-items-center p-8" >
-      <ul className="w-[50vw] flex flex-col justify-center items-center gap-4">
+      <ul className="w-[60vw] grid grid-cols-1 sm:grid-cols-2 gap-x-12 gap-y-2">
         { data 
             ?  data.results.map((episode)=>(
                 <Link key={episode.id} to={`${episode.id}`}> 
-                  <li  className="text-gray-800 dark:text-gray-200 bg-gray-200 bg-gradient-to-r hover:bg-gradient-to-l from-gray-300 to-gray-100 border
+                  <li  className="list-decimal text-gray-800 dark:text-gray-200 bg-gray-200 bg-gradient-to-r hover:bg-gradient-to-l from-gray-300 to-gray-100 border
                            dark:from-gray-800 dark:to-gray-600 hover:shadow-md transition-all hover:font-bold p-2 rounded-[0.5rem]">
                     {episode.name}
                   </li>
