a1298e20d848971cf5aab658839a287857871fc6
Reisel Valle <reiselvalle@gmail.com>
2023-11-20 10:03:32 -0500

    darktheme layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="p-5 bg-gray-100 min-h-screen">
+    <div className="p-5 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 min-h-screen">
       <Outlet />
     </div>
   )
